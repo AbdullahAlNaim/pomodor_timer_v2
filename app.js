35885e36fa0c4846e3bf6bc61e1bc2b4cb83bc04
Bucker000 <aanaim96@gmail.com>
2023-07-23 10:06:43 -0400

    applied some time counter

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const time = document.querySelector('.time')
-
+const start = document.querySelector('.start-stop');
+const press = document.querySelector('.press');
 // let test = new Date()
 // let test 2 =
 // console.log(Date.now());
@@ -12,14 +13,30 @@ const minutes = Math.floor((mili / 1000 / 60) % 60);
 
 const hours = Math.floor((mili / 1000 / 60 / 60) % 24);
 
+let active = true;
+
 let test = Date.now();
-let x = setInterval(function () {
-    let now = new Date().getTime();
-    let sec = Math.floor(((now - test) / 1000) % 60);
-    let min = Math.floor(((now - test) / 1000 / 60) % 60)
-    console.log(min, sec);
-    time.innerHTML = `${min}:${sec}`
-}, 1000)
+function begin() {
+    let x = setInterval(function () {
+        let now = new Date().getTime();
+        let sec = Math.floor(((now - test) / 1000) % 60);
+        let min = Math.floor(((now - test) / 1000 / 60) % 60)
+        console.log(min, sec);
+        time.innerHTML = `${min}:${sec}`
+    }, 1000)
+}
+
+start.addEventListener('click', () => {
+    begin();
+    active = !active;
+    if (active) {
+        press.innerHTML = 'STOP'
+    } else {
+        press.innerHTML = 'START'
+
+        clearInterval(begin());
+    }
+});
 
 // this is how you use maps
 // let firstHash = new Map([
